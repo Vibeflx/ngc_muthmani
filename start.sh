5bcdbe37e3be57e5713339017be8695300cac862
Vibeflx <102888645+Vibeflx@users.noreply.github.com>
2022-04-03 10:41:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master  https://github.com/Vibeflx/ngc_muthmani /ngc_muthmani
+git clone -b master https://github.com/Vibeflx/ngc_muthmani.git /ngc_muthmani
 cd /ngc_muthmani
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
